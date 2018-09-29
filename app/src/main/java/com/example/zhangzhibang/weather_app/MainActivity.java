19d63d7b60f5dd1678f1c020978dd1ec6f929fbd
zhang920714 <zhangzb2016@pku.edu.cn>
2018-09-30 01:14:30 +0800

    make the first activity&layout,regesister in androidmanifest.xml

---
+++ app/src/main/java/com/example/zhangzhibang/weather_app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.zhangzhibang.weather_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/*
+created by zhangzhibang on 18/9/30
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
